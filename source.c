0c0b135f8dda22292baf322381e9ca7fd203e0db
boldbug <boldbug9@gmail.com>
2026-07-21 17:18:59 +0530

    feat : implement a function to get the offset of starting line

--- source.c
+++ source.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <errno.h>
 
 int get_linecount(const char *filename){
     FILE *fp;
@@ -18,10 +19,7 @@ int get_linecount(const char *filename){
     while((ch = fgetc(fp)) != EOF){
         file_empty = false;
         if(ch == '"'){
-            inQuote = 1;
-        }
-        if(inQuote && ch=='"'){
-            inQuote = 0;
+            inQuote = !inQuote;
         }
 
         if(!inQuote && ch=='\n'){
@@ -36,29 +34,56 @@ int get_linecount(const char *filename){
     return lines;
 }
 
-char *get_range_content(int start,int end){
-    //returns content withing a certain range
+long return_line_offset(const char *filename,int target_line){
+    int lines = get_linecount(filename);
+    if(target_line > lines || target_line < 1){
+        fprintf(stdout,"Out of bounds\n");
+        return -1;
+    }
+    FILE *fp = fopen(filename,"r");
+    if(!fp){
+        fprintf(stdout,"Error : %s",strerror(errno));
+        return -1;
+    }
+
+    int ch;
+    int current_line = 1;
+    bool inQuote = false;
+    bool file_empty = true;
+    int line_tracker = 0;
+    bool line_found = false;
+    while ((ch = fgetc(fp)) != EOF) {
+        if (ch == '"') {
+            inQuote = !inQuote;
+        }
+        if (!inQuote && ch == '\n') {
+            current_line++;
+            if (current_line == target_line) {
+                long offset = ftell(fp); // Location of start of target line
+                fclose(fp);
+                return offset;
+            }
+        }
+    }
+    fclose(fp);
+    return -1;
+}
+char *get_range_content(const char *filename,int start,int end){
+    //return content withing a certain range
 }
 
 int main(void){
     const char *filename = "source.c";
     int lines = get_linecount(filename);
-    int line_range_start = 11;
-    int line_range_end = 0;
 
-    if(line_range_end > line_range_start){
-        if(line_range_end > lines || line_range_start < 0){
-            if(line_range_start < 0){
-                fprintf(stdout,"Error: line range start cannot be less than 0\n");
-            }
-            if(line_range_end > lines){
-            fprintf(stdout,"Error: Invalid line range , this file contains only %d lines\n",lines);
-        }
-        return -2;
-        }
-    }else{
-        fprintf(stdout,"Invalid line range;start needs to be less than end\n");
-        return -2;
+    int start = 1;
+    int end = 5;
+
+    if (start < 1 || end > lines || start > end) {
+        fprintf(stderr, "Invalid range [%d to %d] for file with %d lines\n", start, end, lines);
+        return -1;
     }
+
+
     return 0;
 }
